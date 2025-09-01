fc838b2aae0bd983e5ecac65f71f5f990913ee35
Ushakov Aleksandr <alexgo299999@gmail.com>
2025-09-01 22:46:46 +0300

    trying implement NotificationListener

--- app/src/main/java/com/x_xsan/signalcore/NotificationListener.kt
+++ app/src/main/java/com/x_xsan/signalcore/NotificationListener.kt
@@ -1,4 +1,24 @@
 package com.x_xsan.signalcore
 
-class NotificationListener {
+import android.service.notification.NotificationListenerService
+import android.service.notification.StatusBarNotification
+import android.util.Log
+
+class NotificationListener : NotificationListenerService() {
+
+    override fun onNotificationPosted(sbn: StatusBarNotification?) {
+        super.onNotificationPosted(sbn)
+
+        if (sbn != null) {
+            sbn.notification.extras.getString("android.title")
+            val packageName = sbn.packageName
+            val title = sbn.notification.extras.getString("android.title")
+
+            Log.d("NotificationListener", "Notification Posted! Package: $packageName, Title: $title")
+        }
+    }
+
+    override fun onNotificationRemoved(sbn: StatusBarNotification?) {
+        super.onNotificationRemoved(sbn)
+    }
 }
