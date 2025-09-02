e0ff6daf6bfd2d006261149b68a6ec73df3b6864
Ushakov Aleksandr <alexgo299999@gmail.com>
2025-09-02 09:23:00 +0300

    NotificationListener implemented

--- app/src/main/java/com/x_xsan/signalcore/NotificationListener.kt
+++ app/src/main/java/com/x_xsan/signalcore/NotificationListener.kt
@@ -3,22 +3,38 @@ package com.x_xsan.signalcore
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
 import android.util.Log
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.first
 
 class NotificationListener : NotificationListenerService() {
+
+    private val serviceScope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+    private val userPreferencesRepository: UserPreferencesRepository by lazy {
+        UserPreferencesRepository(applicationContext)
+    }
 
     override fun onNotificationPosted(sbn: StatusBarNotification?) {
         super.onNotificationPosted(sbn)
 
-        if (sbn != null) {
-            sbn.notification.extras.getString("android.title")
-            val packageName = sbn.packageName
+        if (sbn != null && sbn.packageName == "org.telegram.messenger") {
             val title = sbn.notification.extras.getString("android.title")
+            if (title != null) {
+                serviceScope.launch {
+                    val priorityContacts = userPreferencesRepository.priorityContacts.first()
 
-            Log.d("NotificationListener", "Notification Posted! Package: $packageName, Title: $title")
+                    Log.d("NotificationListener", "Checking notification. Title: '$title'. Priority list: $priorityContacts")
+
+                    if (title in priorityContacts) {
+                        Log.d("NotificationListener", "!!! MATCH FOUND: $title !!!")
+                        // TODO: Здесь будет код для запуска звука, вибрации и фонарика!
+                    }
+                }
+            }
         }
     }
 
-    override fun onNotificationRemoved(sbn: StatusBarNotification?) {
-        super.onNotificationRemoved(sbn)
+    override fun onDestroy() {
+        super.onDestroy()
+        serviceScope.cancel()
     }
 }
